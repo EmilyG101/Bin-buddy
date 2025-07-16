9e8d1d645fee494fd0dc9f954acc30504ec09601
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2025-07-16 19:05:53 -0400

    Update subscription.js

--- subscription.js
+++ subscription.js
@@ -1,1 +1,5 @@
+function selectPlan(planName) {
+  alert(`Thank you for choosing the ${planName} Plan! You'll start earning points and saving soon.`);
+  // Here you can extend this to call backend subscription/payment APIs
+}
 
